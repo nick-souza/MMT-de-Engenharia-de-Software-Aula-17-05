4a9d7fc909e64ef8f12ff1375f2603df90ff1718
Nicolas Sa de Souza <43567322+nsds26@users.noreply.github.com>
2022-05-17 20:42:54 -0300

    Add files via upload

---
+++ MementoPatternDemo/src/mementopatterndemo/CareTaker.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mementopatterndemo;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Nicolas Sá de Souza
+ */
+public class CareTaker{
+   private List<Memento>mementoList = new ArrayList<Memento>();
+   
+   public void add(Memento state){
+      mementoList.add(state);
+  }
+   public Memento get(int index){
+      return mementoList.get(index);
+  }
+}
